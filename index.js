726ab4f60f83b5dc37d3fe200a6b602a30ec3597
Pistoja <mans96@hotmail.it>
2019-04-16 11:00:51 +0200

    cambio messaggio

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const http = require('http');
 
 const server = http.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
-res.write('Ciao mondo!');
+res.write('Nome Cognome!');
 res.end();
 })
 //in ascolto
-// server listen
+// server.listen(porta, url)
 server.listen(8081, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:8081/');
