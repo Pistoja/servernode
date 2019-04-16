9e1e8627d072feb89d46089c29c95ee034e9f58d
Pistoja <mans96@hotmail.it>
2019-04-16 10:24:49 +0200

    Primo server node.js

---
+++ index.js
@@ -0,0 +1,17 @@
+//semplice web server
+// modulo http
+
+const http = require('http');
+
+// comportamento web server con callback
+
+const server = http.createServer(function(req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write('Ciao mondo!');
+res.end();
+})
+//in ascolto
+// server listen
+server.listen(8080, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:8080/');
