f65683fabcf817767bcb8addd0ce6ffad73d16fc
Pistoja <mans96@hotmail.it>
2019-04-19 10:27:47 +0200

    create step1

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
-//semplice web server
-// modulo http
+const express = require('express')
+let port = process.argv[3] || 3000
+const app = express() 
 
-const http = require('http');
-const port = 8081;
-
-// comportamento web server con callback
-
-const server = http.createServer(function(req, res) {
-    console.log(req.url)
-    if (req.url == '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>Home</h1>');     
-   
-        res.end();
-
-
-    } else if (req.url == '/api') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(JSON.stringify(
-            [
-              {  "nome": "Mario",
-                 "cognome": "Rossi"
-              },
-              {  "nome": "Ken",
-               "cognome": "Norris"
-              }
-
-            ]
-        ));        
-        res.end();
-
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>${req.url}</h1>');        
-        res.end();
-    }
-});
-//in ascolto
-// server.listen(porta, url)
-server.listen(port, '127.0.0.1');
-
+if(process.argv[2] === 'v') {
+  console.log('MyApp versione 0.0.1')
+}
+app.get('/', function (req, res) {
+  res.send('Hello World')
+}) 
+app.listen(port)
 console.log(`Server running at http://127.0.0.1:${port}`);
