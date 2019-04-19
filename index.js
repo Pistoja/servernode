b327eaab205cb88f106bd96b3fd6980d8839711d
Pistoja <mans96@hotmail.it>
2019-04-19 12:06:50 +0200

    add fs

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
-//let port = process.argv[2] || 3000
-let port = process.argv[2] || 3000
 const app = express() 
+const fs = require('fs')
+let port = process.argv[2] || 8080
 
-//app.use(express.static('public'))
-app.use(express.static(__dirname + '/public'))
-
-//app.get('/', (req, res) => {
-//  res.send('Hello World')
-//}) 
-
+app.get('/books', function(req, res)
+{
+    const filename = process.argv[3]
+    fs.readFile(filename, function(e, data)
+    {   // 500 Internal Server Error
+        if (e) return res.sendStatus(500)
+        try
+        {
+            books = JSON.parse(data)
+        } catch (e)
+        {
+            res.sendStatus(500)
+        }
+        res.json(books)
+    })
+})
 app.listen(port)
 console.log(`Server running at http://127.0.0.1:${port}`);
