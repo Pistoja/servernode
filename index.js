0a03130bd6f30d6ab15f6a097da54fe1c9201c44
Pistoja <mans96@hotmail.it>
2019-04-19 12:20:20 +0200

    add module fs

--- index.js
+++ index.js
@@ -19,5 +19,24 @@ app.get('/books', function(req, res)
         res.json(books)
     })
 })
+
+app.get('/menu', function(req, res)
+{
+    const filename = process.argv[3]
+    fs.readFile(filename, function(e, data)
+    {   // 500 Internal Server Error
+        if (e) return res.sendStatus(500)
+        try
+        {
+            menu = JSON.parse(data)
+        } catch (e)
+        {
+            res.sendStatus(500)
+        }
+        res.json(menu)
+    })
+})
+
+
 app.listen(port)
-console.log(`Server running at http://127.0.0.1:${port}`);
+console.log(`Server running at http://127.0.0.1:${port}/menu`);
