53af09977395ee941ca1f4f63903b2d77c0c2047
Pit <mans96@hotmail.it>
2019-05-07 10:29:30 +0200

    console log

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const {settings} = require('./settings.js')
 const users = require('./routes/users')
 const personaggi = require('./routes/personaggi')
 let port = process.argv[2] || 7070
+
+const myLogger = (req, res, next) => {
+    console.log('LOGGED');
+    next();
+}
+
+app.use(myLogger);
+
 app.use('/users', users)
 app.use('/personaggi', personaggi)
 
