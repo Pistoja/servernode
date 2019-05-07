ef0bcd88f37aa922bac56b3e5a69f76e4c70f91c
Pit <mans96@hotmail.it>
2019-05-07 10:18:17 +0200

    port 7070

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const {settings} = require('./settings.js')
 const users = require('./routes/users')
 const personaggi = require('./routes/personaggi')
-let port = process.argv[2] || 8080
+let port = process.argv[2] || 7070
 app.use('/users', users)
 app.use('/personaggi', personaggi)
 
