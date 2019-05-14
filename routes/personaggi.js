cd059cc9d213dd3e123c5438e579b064c96ec288
Pit <mans96@hotmail.it>
2019-05-14 09:42:52 +0200

    mongodb fix

--- routes/personaggi.js
+++ routes/personaggi.js
@@ -3,25 +3,9 @@ const router = express.Router();
 const characters = require('../data/characters')
 // QUery
 // chiedi http://localhost:7070/personaggi?colore=giallo&sesso=m
-router.get('/', (req, res, next) => {
-    const query = req.query
-    let personaggi = characters.characters
-    if ('lastname' in query) {
-      personaggi = personaggi.filter((personaggio) => {
-        return personaggio.lastname === query.lastname
-      })
-    }
-    if ('firstname' in query) {
-      personaggi = personaggi.filter((personaggio) => {
-        return personaggio.firstname === query.firstname
-      })
-    }
-    res.send(personaggi)
-    // next();
-  }, (req, res, next) => {
-    console.log('FIRE 2')
-  }
-)
+router.get('/', (req, res, next) => {    
+    res.send(characters)    
+});
 
 // Params
 //http://localhost:7070/personaggi/9
